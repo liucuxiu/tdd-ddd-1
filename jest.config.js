caa7cdc80437845515f75c6d06061a9397286814
liucuxiu <xliucuxiux@gmail.com>
2023-05-12 23:35:53 +0700

    test CI/CD

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,17 @@
+// eslint-disable-next-line no-undef
 module.exports = {
   transform: {
-    '^.+\\.ts?$': 'ts-jest'
+    '^.+\\.ts?$': [ 'ts-jest',{
+      'ts-jest': {
+        'tsconfig': 'tsconfig.json'
+      }
+    }
+    ]
   },
   testEnvironment: 'node',
   testRegex: './src/.*\\.(test|spec)?\\.(ts|ts)$',
   moduleFileExtensions: [ 'ts', 'tsx', 'js', 'jsx', 'json', 'node' ],
   roots: [
     '<rootDir>/src'
-  ]
+  ],
 };
